23023935655171a39626bf8bd49bf4ea798c9bfe
Vedant1607 <sinhavedant0705@gmail.com>
2025-09-14 00:28:31 +0530

    minor changes

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,7 +1,12 @@
-export function InputComponent({onChange, placeholder}:{onChange: () => void, placeholder:string}) {
+interface InputProps {
+  placeholder:string;
+  ref?:any;
+}
+
+export function InputComponent({placeholder, ref}:InputProps) {
   return  (
     <div>
-      <input placeholder={placeholder} type="text" className="px-4 py-2 border rounded m-2" onChange={onChange}/>
+      <input ref={ref} placeholder={placeholder} type="text" className="px-4 py-2 border rounded m-2"/>
     </div>
   )
 }
