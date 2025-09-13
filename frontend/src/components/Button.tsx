1e6e496e2c4a5484e843164f738d3198f6e14e59
Vedant1607 <sinhavedant0705@gmail.com>
2025-09-14 00:26:32 +0530

    Button Component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,7 +4,9 @@ interface ButtonProps {
   variant: "primary" | "secondary";
   text: string;
   startIcon?: ReactElement;
-  onClick?: () => void
+  onClick?: () => void;
+  fullWidth?: boolean;
+  loading?: boolean;
 }
 
 const variantClasses = {
@@ -14,9 +16,22 @@ const variantClasses = {
 
 const defaultStyles = "px-4 py-2 rounded-md font-light flex items-center";
 
-const Button = ({ variant, text, startIcon, onClick }: ButtonProps) => {
+const Button = ({
+  variant,
+  text,
+  startIcon,
+  onClick,
+  fullWidth,
+  loading,
+}: ButtonProps) => {
   return (
-    <button onClick={onClick} className={`${variantClasses[variant]} ${defaultStyles}`}>
+    <button
+      onClick={onClick}
+      className={`${variantClasses[variant]} ${defaultStyles} ${
+        fullWidth ? "w-full flex justify-center items-center" : ""}
+        ${loading ? "opacity-45" : ""}`}
+      disabled={loading}
+    >
       <div className="pr-2">{startIcon}</div>
       {text}
     </button>
