afb62412cdf2bce83f63125b46972b5c37f182dd
Vedant1607 <sinhavedant0705@gmail.com>
2025-09-14 00:29:21 +0530

    needs bugs fixing

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,18 +1,38 @@
+import { useRef } from "react";
 import Button from "../components/Button";
 import { InputComponent } from "../components/Input";
+import axios from 'axios';
+import { BACKEND_URL } from "../config";
 
 export function Signup() {
+  const usernameRef = useRef<HTMLInputElement>()
+  const passwordRef = useRef<HTMLInputElement>()
+
+  async function signup() {
+    const username = usernameRef.current?.value;
+    const password = usernameRef.current?.value;
+    await axios.post(`${BACKEND_URL}/api/v1/auth/signup`, {
+      data: {
+        username,
+        password
+      }
+    });
+    console.log(usernameRef, passwordRef)
+    alert("You have signed up");
+  }
+
   return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
       <div className="bg-white rounded flex flex-col justify-center items-center p-8">
-        <InputComponent placeholder="Username" />
-        <InputComponent placeholder="Password" />
+        <InputComponent ref={usernameRef} placeholder="Username" />
+        <InputComponent ref={passwordRef} placeholder="Password" />
 
         <Button
           variant="primary"
           text="Signup"
           fullWidth={true}
           loading={false}
+          onClick={signup}
         />
       </div>
     </div>
